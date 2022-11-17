f27e58303beffa68f8d37f16efcac9493e5ecc6e
MiguelAngelDLA <miguelangeldla14@gmail.com>
2022-11-17 13:21:40 -0600

    SQL Corregido

--- ProyectoT5FBD.sql
+++ ProyectoT5FBD.sql
@@ -3,14 +3,19 @@ use empresa;
 
 -- tabla bebida(nombre(PK), tamaño, tipo de leche, tipo de azúcar, ingredientes, forma de preparación)
 create table bebida(
-nombreB			varchar(30) primary key unique,
+claveB int8 primary key unique,
+nombreB			varchar(30),
 tamaño			varchar(20),
 tipoDeLeche		varchar(30),
 tipoDeAzucar	varchar(30),
 ingredientes	varchar(80),
-formPreparacion	varchar(80)
+formPreparacion	varchar(80),
+rfcCliente 		varchar(20),
+curpEmp			varchar(20),
+foreign key (rfcCliente) references cliente(RFC),
+foreign key(curpEmp) references Empleado(curpEmp)
 );
-
+-- Llave cliente y empleado en la bebida
 -- tabla cliente(RFC(PK), nombre, fechaDeCompra, numeroDeB, tipoDeB, totalPago)
 create table cliente(
 RFC				varchar(20) primary key unique,
@@ -21,9 +26,7 @@ apMatC			varchar(30),
 fechaDeCompra	datetime,
 numeroDeB		int8,
 tipoDeB			varchar(30),
-totalPago		double,
-nombreB			varchar(30),
-foreign key (nombreB) references bebida(nombreB)
+totalPago		double
 );
 
 -- tabla proveedores(RFCP(PK), nombre, numProdProv, fehcaEnvio, cantFact), proveedor aprobado
@@ -56,9 +59,7 @@ apPatE		varchar(30),
 apMatE		varchar(30),
 celular		int8,
 ocupacion	varchar(30),
-hrsTrab		int8,
-nombreB		varchar(30),
-foreign key (nombreB) references bebida(nombreB)
+hrsTrab		int8
 );
 
 -- tabla gerente(CURPG(PK), nombre, dirección, telCasa, celularG, sueldo)
@@ -84,25 +85,25 @@ CURPG		varchar(20),
 RFCP		varchar(20),
 CURPE		varchar(20),
 RFC			varchar(20),
-nombreB		varchar(30),
+claveB		int8,
 foreign key (CURPG) references gerente(CURPG),
 foreign key (RFCP) references proveedores(RFCP),
 foreign key (CURPE) references empleado(CURPE),
 foreign key (RFC) references cliente(RFC),
-foreign key (nombreB) references bebida(nombreB)
+foreign key (claveB) references bebida(claveB)
 );
 -- registros de comprobación
 -- bebida
-insert into bebida values("capuccino", "chico", "entera", "morena", "leche, cafe, azúcar", "calentar agua, mezclar el agua, cafe y azúcar al gusto");
+insert into bebida values(12345,"capuccino", "chico", "entera", "morena", "leche, cafe, azúcar", "calentar agua, mezclar el agua, cafe y azúcar al gusto");
 -- cliente
-insert into cliente values("FIR230943", "Isaac", "Neftali", "Burciaga", "Chacón", '2022-11-15', 10, "café", 1000, "capuccino");
+insert into cliente values("FIR230943", "Isaac", "Neftali", "Burciaga", "Chacón", '2022-11-15', 10, "café", 1000, 12345);
 -- proveedorAprobado
 insert into proveedorAprobado values("PACW3424", "Rodrigo", "", "Macías", "Ruiz");
 -- proveedores
 insert into proveedores values("PFWE25233", "José", "Emanuel", "Guerrero", "Segura", 300, '2022-11-01', 6000, "PACW3424");
 -- empleado (hrsTrab es por semana)
-insert into empleado values("EMP32425", "Kevin", "Zaid", "Landeros", "Cruz", 8712334567, "producción", 40, "capuccino");
+insert into empleado values("EMP32425", "Kevin", "Zaid", "Landeros", "Cruz", 8712334567, "producción", 40, 12345);
 -- gerente
 insert into gerente values("GDT45663", "Juan", "Manuel", "Torres", "Ramírez", "Campo de magnolias", "855", "Campo nuevo", "27982", "Torreón", 2350252, 8715334570, 30000);
 -- franquicia
-insert into franquicia values("GDT45663", "PFWE25233", "EMP32425", "FIR230943", "capuccino");
+insert into franquicia values("GDT45663", "PFWE25233", "EMP32425", "FIR230943", 12345);
